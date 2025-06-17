518dda724bc18c44d1563a9c1afb2a63f43b4d95
Tashar999 <27tashar@cpsd.us>
2025-06-17 19:56:46 -0400

    created functions for each operation + operation function to decide operation

---
+++ script.js
@@ -0,0 +1,34 @@
+let a = 0;
+let b = 0;
+let operator = "";
+
+function operate (operator, a, b) {
+    if (operator == '+'){
+        add(a, b)
+    } else if (operator == '-'){
+        subtract(a, b)
+    } else if (operator == '*'){
+        multiply(a, b)
+    } else if (operator == '/'){
+        divide(a, b)
+    }
+};
+
+
+
+function add(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a/b
+}
+
