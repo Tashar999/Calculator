356ed113b3fad632a0c9997e6ea45045d11d731c
Tashar999 <27tashar@cpsd.us>
2025-06-17 22:33:30 -0400

    created functions to take input and return value, fixed extra bugs

--- script.js
+++ script.js
@@ -1,22 +1,77 @@
 let a = 0;
 let b = 0;
 let operator = "";
+let result = null
+const display = document.getElementById('display')
 
 function clearDisplay () {
-    const display = document.getElementById('display')
     display.textContent = ""
+    a = null;
+    b = null;
+    operator = "";
+    result = null
+}
+
+function addToDisplay (input) {
+    
+    if (result != null) {
+        clearDisplay();
+    }
+    display.textContent += input
+}
+
+function operatorChoice (chosenOperator){
+    if (display.textContent === "") {
+        operator = chosenOperator;
+        return;
+    }
+    if (result != null) {
+        a = result;
+        result = null
+    }
+    if (operator && a !== null && display.textContent !== "") {
+        b = display.textContent;
+        result = operate(operator, parseFloat(a), parseFloat(b));
+        if (typeof result === "number" && !Number.isInteger(result)) {
+            result = parseFloat(result.toFixed(4));
+//   some digit function thing
+        }
+        display.textContent = result;
+        a = result;
+    } else if (display.textContent !== ""){
+        a = display.textContent;
+    }
+    operator = chosenOperator;
+    display.textContent = "";
+}
+
+function calc () {
+    if (a === null || !operator) return;
+    if (display.textContent === "") {
+        b = a;
+    } else {
+        b = display.textContent
+    }
+    result = operate(operator, parseFloat(a), parseFloat(b))
+    if (typeof result === "number" && !Number.isInteger(result)) {
+        result = parseFloat(result.toFixed(4));
+    }
+    display.textContent = result
+    a = result
+    b = null;
+    operator = "";
 }
 
 
 function operate (operator, a, b) {
     if (operator == '+'){
-        add(a, b)
+        return add(a, b)
     } else if (operator == '-'){
-        subtract(a, b)
+        return subtract(a, b)
     } else if (operator == '*'){
-        multiply(a, b)
+        return multiply(a, b)
     } else if (operator == '/'){
-        divide(a, b)
+        return divide(a, b)
     }
 };
 
